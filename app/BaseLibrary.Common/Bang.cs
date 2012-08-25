766a1078752f12bbeea27956ce14228df58bbff2
bristm <Marcus.Bristol@airnz.co.nz>
2012-08-25 20:16:17 +1200

    Added another project to better simulate the issue

---
+++ app/BaseLibrary.Common/Bang.cs
@@ -0,0 +1,14 @@
+﻿namespace BaseLibrary.Common
+{
+    using System;
+    using System.Diagnostics.Contracts;
+
+    public class Bang
+    {
+        public bool Ouch(string shoot)
+        {
+            Contract.Requires<ArgumentNullException>(shoot != null, "shoot");
+            return true;
+        }
+    }
+}
---
+++ app/BaseLibrary.Unittests/BangTests.cs
@@ -0,0 +1,27 @@
+﻿namespace BaseLibrary.Unittests
+{
+    using System;
+
+    using BaseLibrary.Common;
+
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class BangTests
+    {
+        [Test]
+        public void Shoot_NullArgument_ThrowsException()
+        {
+            // Arrange
+            var bang = new Bang();
+
+            // Act
+
+            // Assert
+            Assert.Throws(
+                Is.InstanceOf<ArgumentNullException>().And.Message.StringMatching(
+                    "Precondition failed: shoot != null  shoot\r\nParameter name: shoot"),
+                () => bang.Ouch(null));
+        } 
+    }
+}
